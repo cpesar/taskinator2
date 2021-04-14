552ff7fcbbaaaaf33e01e0c365e1804e006d23e5
cpesar <77510555+cpesar@users.noreply.github.com>
2021-04-13 22:34:28 -0600

    added form element to replace the buttonEL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  
+  event.preventDefault();
   //create a new task item
   var listItemEl = document.createElement("li");
   //style the new task
@@ -12,6 +14,9 @@ var createTaskHandler = function() {
   listItemEl.textContent = "This is a new task.";
   //append this element to the task list
   tasksToDoEl.appendChild(listItemEl);
+
+  console.log(event);
+ 
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
